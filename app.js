fe357e983ac4a13a4b0aed0e97026708fdc9febc
febyputra <93692256+febyputra@users.noreply.github.com>
2023-02-27 15:54:33 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// Mengambil Elemen Gambar dan Tombol
+var img = document.querySelector('.slideshow');
+var prevBtn = document.querySelector('.prev-btn');
+var nextBtn = document.querySelector('.next-btn');
+
+// Membuat Array dengan Beberapa Sumber Gambar
+var imgSources = ['/assets/1.jpg', '/assets/2.jpg', '/assets/3.jpg'];
+
+// Menentukan Indeks Gambar Saat Ini
+var currentIndex = 0;
+
+// Fungsi untuk Mengganti Gambar
+function changeImage() {
+    img.src = imgSources[currentIndex];
+}
+
+// Fungsi untuk Menggeser ke Gambar Sebelumnya
+function prevImage() {
+    currentIndex = (currentIndex - 1 + imgSources.length) % imgSources.length;
+    changeImage();
+}
+
+// Fungsi untuk Menggeser ke Gambar Berikutnya
+function nextImage() {
+    currentIndex = (currentIndex + 1) % imgSources.length;
+    changeImage();
+}
+
+// Menambahkan Event Listener ke Tombol Geser
+prevBtn.addEventListener('click', prevImage);
+nextBtn.addEventListener('click', nextImage);
+
+// Menjalankan Fungsi untuk Replace Gambar setiap 3 detik
+(nextImage, 1000);
